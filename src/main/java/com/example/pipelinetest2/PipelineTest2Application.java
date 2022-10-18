eb3e694244152db4d464fd8c7a796bd28d1b1f04
harvey.c <harvey.c@kakaoenterprise.com>
2022-10-18 20:45:19 +0900

    Initial commit

---
+++ src/main/java/com/example/pipelinetest2/PipelineTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.pipelinetest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pipelinetest2/PipelineTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pipelinetest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
